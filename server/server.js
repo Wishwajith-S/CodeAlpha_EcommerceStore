eb15fc49faf369b42dd718c4d3ae5a06e0d82316
Wishwajith_S <wishwajith020406@gmail.com>
2026-06-07 11:49:28 +0530

    Connected frontend with backend

--- server/server.js
+++ server/server.js
@@ -1,29 +1,69 @@
 const express = require("express");
+
 const cors = require("cors");
+
+const path = require("path");
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
+const PORT = process.env.PORT || 5000;
 
 
 // IMPORT ROUTES
-const productRoutes = require("./routes/productRoutes");
-const authRoutes = require("./routes/authRoutes");
-// USE ROUTES
-app.use("/api/products", productRoutes);
-app.use("/api/auth", authRoutes);
 
-// TEST ROUTE
+const productRoutes =
+require("./routes/productRoutes");
+
+const authRoutes =
+require("./routes/authRoutes");
+
+
+// MIDDLEWARE
+
+app.use(cors());
+
+app.use(express.json());
+
+
+// SERVE FRONTEND FILES
+
+app.use(
+    express.static(
+        path.join(__dirname, "../client")
+    )
+);
+
+
+// API ROUTES
+
+app.use(
+    "/api/products",
+    productRoutes
+);
+
+app.use(
+    "/api/auth",
+    authRoutes
+);
+
+
+// DEFAULT ROUTE
+
 app.get("/", (req, res) => {
-    res.send("E-Commerce Backend Running...");
+
+    res.sendFile(
+        path.join(__dirname, "../client/index.html")
+    );
+
 });
 
-// PORT
-const PORT = process.env.PORT || 5000;
+
+// START SERVER
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+    console.log(
+        `Server running on port ${PORT}`
+    );
+
 });
